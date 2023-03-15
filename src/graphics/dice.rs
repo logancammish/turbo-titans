608afda3f4df578189eaa20e3def709a4ce8931d
Logan Cammish <124637378+logancammish@users.noreply.github.com>
2023-03-15 21:19:49 +1300

    Update dice.rs

---
+++ src/graphics/dice.rs
@@ -0,0 +1,14 @@
+pub fn generate() -> usize {
+    let die: [&str; 6] = [
+        "__________\n|        |\n|    *   |\n|        |\n|________|",
+        "__________\n|        |\n|  *  *  |\n|        |\n|________|",
+        "__________\n|        |\n|  *  *  |\n|    *   |\n|________|",
+        "__________\n|        |\n|  *  *  |\n|  *  *  |\n|________|",
+        "__________\n|        |\n| * * *  |\n| *   *  |\n|________|",
+        "__________\n| *   *  |\n| *   *  |\n| *   *  |\n|________|",
+    ];
+    let rand_int: usize = rand::prelude::thread_rng().gen_range(0..5);
+    println!("You rolled {}:\n{}\r", rand_int + 1, die[rand_int].trim());
+    
+    return rand_int;
+}
